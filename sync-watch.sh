74b619577fe2fe7cb2eaf2075d42a7c6a1c8ca66
Joseley Rebelo <joseley94@gmail.com>
2023-09-20 01:22:10 +0100

    Update and rename jwatch.sh to sync-watch.sh

--- sync-watch.sh
+++ sync-watch.sh
@@ -1,13 +1,13 @@
 #!/bin/bash
 
-jwatch(){
+sync-watch(){
     if [ -d "$1" ]; then 
         fswatch --batch-marker=EOF -xn $1 | while read file event; do 
             if [ $file = "EOF" ]; then 
                 if [ -d "$2" ]; then 
                     rsync -rtu --delete $1 $2
                     now=$(date +"%r")
-                    tput rev; echo "Running jwatch end-goal. (${now})" ; tput sgr0
+                    tput rev; echo "Running sync-watch end-goal. (${now})" ; tput sgr0
                 else 
                     echo "Second argument must be a directory."
                 fi
